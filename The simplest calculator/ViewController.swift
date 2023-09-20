2022ed975b7c00d58042621e1fb4aec4eedc2c15
Maksym-Bohun <maks.boggun@gmail.com>
2023-09-20 14:39:04 +0300

    finished app

--- The simplest calculator/ViewController.swift
+++ The simplest calculator/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumClicked(_ sender: UIButton) {
+        if  let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                
+                result =  firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: UIButton) {
+        if  let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: UIButton) {
+        if  let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: UIButton) {
+        if  let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
 }
 
